a4150cc9c9dba45a6ebf10738a62c94be2eac725
Smite Chow <xiaopengyou2no1@gmail.com>
2024-11-22 23:00:12 +0800

    suit for app.hukoubook.com

--- src/state/session/moderation.ts
+++ src/state/session/moderation.ts
@@ -38,7 +38,7 @@ export async function configureModerationForAccount(
 }
 
 function switchToBskyAppLabeler() {
-  BskyAgent.configure({appLabelers: [BSKY_LABELER_DID]})
+  BskyAgent.configure({appLabelers: [BSKY_LABELER_DID, 'did:plc:dya73iwnrfdedgadx3hrulzl']})
 }
 
 async function trySwitchToTestAppLabeler(agent: BskyAgent) {
