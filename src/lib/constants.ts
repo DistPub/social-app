8a1e140b55cfae3a71db24476f0be6f820b7264e
Smite Chow <xiaopengyou2no1@gmail.com>
2025-12-05 15:15:46 +0800

    support app.bsky.graph.getSuggestedFollowsByActor

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -254,6 +254,7 @@ export const FATESKY_SUPPORT_XRPC_LXM = [
   'app.bsky.actor.searchActors',
   'app.bsky.unspecced.getPopularFeedGenerators',
   'app.bsky.feed.getPosts',
+  'app.bsky.graph.getSuggestedFollowsByActor',
 ]
 export function useFateskyAppview(...args) {
   let req = new globalThis.Request(...args)
